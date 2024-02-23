a3b8900536f929d4f302e1911ae99ddf8df61afe
Jose David <lujan9495@gmail.com>
2024-02-23 18:34:21 -0500

    Formatear numeros a dinero

--- src/components/ControlPresupuesto.jsx
+++ src/components/ControlPresupuesto.jsx
@@ -1,11 +1,27 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable react/prop-types */
 const ControlPresupuesto = ({ presupuesto }) => {
+
+    const formatearCantidad = (cantidad) => {
+        return cantidad.toLocaleString('es-CO', {
+            style: 'currency',
+            currency: 'COP'
+        })
+    }
+
   return (
     <div className="contenedor-presupuesto contenedor sombra dos-columnas">
         <p>Grafica aquí</p>
 
         <div className="contenido-presupuesto">
             <p>
-                <span>Presupuesto: </span> ${presupuesto} 
+                <span>Presupuesto: </span> {formatearCantidad(presupuesto)} 
+            </p>
+            <p>
+                <span>Disponible: </span> {formatearCantidad(0)} 
+            </p>
+            <p>
+                <span>Gastado: </span> {formatearCantidad(0)} 
             </p>
         </div>
     </div>
