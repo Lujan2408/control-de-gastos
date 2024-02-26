a49eb21d24bd68b6d13efa5af10486a2e2d3e136
Jose David <lujan9495@gmail.com>
2024-02-25 23:36:46 -0500

    formateando las fechas

--- src/components/Gasto.jsx
+++ src/components/Gasto.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
+import { formatearFecha } from '../helpers'
 
 const Gasto = ({ gasto }) => {
-    const { nombreGasto, categoria, cantidadGasto, id } = gasto 
+    const { nombreGasto, categoria, cantidadGasto, id, fecha } = gasto 
 
   return (
     <div className='gasto sombra'>
@@ -10,9 +11,14 @@ const Gasto = ({ gasto }) => {
             <div className='descripcion-gasto'>
                 <p className='categoria'>{categoria}</p>
                 <p className='nombre-gasto'>{nombreGasto}</p>
+                <p className="fecha-gasto">
+                    Agregado el: {''}
+                    <span>{formatearFecha(fecha)}</span>
+                </p>
             </div>
-
         </div>
+
+        <div className='cantidad-gasto'>${cantidadGasto}</div>
     </div>
   )
 }
