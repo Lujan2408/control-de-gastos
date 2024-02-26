af5d9bdf565f4d7f478c45fce60073732b84e755
Jose David <lujan9495@gmail.com>
2024-02-26 11:08:39 -0500

    Mostrando el icono de gastos

--- src/components/Gasto.jsx
+++ src/components/Gasto.jsx
@@ -1,6 +1,25 @@
 import React from 'react'
 import { formatearFecha } from '../helpers'
 
+import iconoAhorro from '../assets/icono_ahorro.svg'
+import iconoCasa from '../assets/icono_casa.svg'
+import iconoComida from '../assets/icono_comida.svg'
+import iconoGastos from '../assets/icono_gastos.svg'
+import iconoOcio from '../assets/icono_ocio.svg'
+import iconoSalud from '../assets/icono_salud.svg'
+import iconoSuscripciones from '../assets/icono_suscripciones.svg'
+
+const diccionarioIconos = {
+    ahorro: iconoAhorro,
+    casa: iconoCasa, 
+    comida: iconoComida, 
+    gastos: iconoGastos,
+    ocio: iconoOcio,
+    salud: iconoSalud,
+    suscripciones: iconoSuscripciones
+
+}
+
 const Gasto = ({ gasto }) => {
     const { nombreGasto, categoria, cantidadGasto, id, fecha } = gasto 
 
@@ -8,6 +27,11 @@ const Gasto = ({ gasto }) => {
     <div className='gasto sombra'>
         <div className='contenido-gasto'>
             
+            <img 
+                src={diccionarioIconos[categoria]} 
+                alt="Icono Gasto" 
+            />
+
             <div className='descripcion-gasto'>
                 <p className='categoria'>{categoria}</p>
                 <p className='nombre-gasto'>{nombreGasto}</p>
